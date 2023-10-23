be754768594d92ff703363007b9ec4aabc7f6d07
Taishiyy <taishi.y.mj@gmail.com>
2023-10-23 16:08:42 +0900

    three.js react-three-fiber box rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/**
 import Link from "next/link"
 
 export default function FirstPost() {
@@ -7,3 +8,51 @@ export default function FirstPost() {
         </h1>
     )
 }
+*/
+import type { NextPage } from "next";
+import React, { useRef, useState } from "react";
+import * as THREE from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+const Home: NextPage = () => {
+  const Box = (props: JSX.IntrinsicElements["mesh"]) => {
+    const ref = useRef<THREE.Mesh>(null!);
+    const [hovered, setHover] = useState(false);
+    const [active, setActive] = useState(false);
+    useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+    useFrame((state, delta) => (ref.current.rotation.y += 0.01));
+    useFrame((state, delta) => (ref.current.rotation.z += 0.02));
+    return (
+      <mesh
+        {...props}
+        ref={ref}
+        scale={active ? 1.5 : 1}
+        onClick={(event) => setActive(!active)}
+        onPointerOver={(event) => setHover(true)}
+        onPointerOut={(event) => setHover(false)}
+      >
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={hovered ? "red" : "black"} />
+      </mesh>
+    );
+  };
+
+  return (
+    <Canvas
+      style={{
+        width: 100 + "vw",
+        height: 100 + "vh",
+        position: "fixed",
+        top: 0,
+        left: 0,
+      }}
+    >
+      <ambientLight />
+      <pointLight position={[0, 0, 10]} />
+      <Box position={[0, 0, 0]} />
+      {/* <Box position={[1.2, 0, 0]} /> */}
+    </Canvas>
+  );
+};
+
+export default Home;
